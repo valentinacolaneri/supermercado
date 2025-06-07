7cef87eb6c4d95b23ecd0475a68a5e4e67eca623
valentinacolaneri <colanerivalen2006@gmail.com>
2025-06-07 11:24:52 -0300

    Add files via upload

---
+++ producto.cs
@@ -0,0 +1,17 @@
+public class Producto
+{
+    public int Codigo { get; set; }
+    public string Nombre { get; set; }
+    public decimal PrecioActual { get; set; }
+    public int Stock { get; set; }
+    public Categoria Categoria { get; set; }
+
+    public Producto(int codigo, string nombre, decimal precio, int stock, Categoria categoria)
+    {
+        Codigo = codigo;
+        Nombre = nombre;
+        PrecioActual = precio;
+        Stock = stock;
+        Categoria = categoria;
+    }
+}
