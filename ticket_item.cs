ef9e1171ca0c3b23ecbab0a14b39be6a176cf663
valentinacolaneri <colanerivalen2006@gmail.com>
2025-06-07 14:22:31 -0300

    Add files via upload

---
+++ ticket_item.cs
@@ -0,0 +1,17 @@
+public class TicketItem
+{
+    public string NombreProducto { get; set; }
+    public decimal PrecioUnitario { get; set; }
+    public int Cantidad { get; set; }
+
+    public decimal Subtotal => Cantidad >= 5
+        ? PrecioUnitario * Cantidad * 0.85m
+        : PrecioUnitario * Cantidad;
+
+    public TicketItem(string nombre, decimal precioUnitario, int cantidad)
+    {
+        NombreProducto = nombre;
+        PrecioUnitario = precioUnitario;
+        Cantidad = cantidad;
+    }
+}
